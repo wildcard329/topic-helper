d6dd51c219bdb1abc46babae1d28d5b228433257
wildcard329 <enkiduwildman@gmail.com>
2024-01-15 12:18:58 -0800

    [init-001] first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import { Layout } from './components/layout'
+import AppRouter from './AppRouter'
+
+const App = () => 
+  <Layout>
+    <AppRouter />
+  </Layout>
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => 
+  <div>
+    <h1>
+      Welcome to Topic Helper!
+    </h1>
+    <p>
+      Please log in to continue
+    </p>
+  </div>
+
+export default Home
