1b80e2e2b43f37ad2d809a5d26fc11fce8956eea
wildcard329 <enkiduwildman@gmail.com>
2024-01-18 23:16:53 -0800

    [top-001] style changes to topics

---
+++ src/pages/TopicViewer.jsx
@@ -0,0 +1,6 @@
+const TopicViewer = () =>
+  <div className="page-wrapper">
+
+  </div>
+
+export default TopicViewer;
--- src/pages/Topics.jsx
+++ src/pages/Topics.jsx
@@ -1,12 +1,14 @@
 import TOPICS from "../assets/mocks/mockTopics.json";
+import { AppButton } from "../components/button";
 
 const Topics = () => 
-  <ul className="page-wrapper">
-    {TOPICS?.map((topic) => <li key={topic.id}><a href="#">{topic.title}</a></li>)}
-    {/* <li><a href=""></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li> */}
-  </ul>
+  <div id="topics-wrapper" className="page-wrapper">
+    <ul className="topics-list">
+      {TOPICS?.map((topic) => <li key={topic.id}><AppButton btnClass={"list-btn"} btnVar={"outlined"} btnColor={"primary"}><a href={`${topic.title}`}>{topic.title}</a></AppButton></li>)}
+    </ul>
+    <div className="page-footer">
+      <AppButton>Add Topic</AppButton>
+    </div>
+  </div>
 
 export default Topics;
